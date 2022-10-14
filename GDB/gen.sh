235179ba1108079a18757735ad4a8e6daee19daa
pei.tang <pei.tang@horizon.ai>
2022-10-14 17:14:49 +0800

    defult

---
+++ GDB/gen.sh
@@ -0,0 +1,7 @@
+# Copyright (C) Horizon Robotics, Inc. - All Rights Reserved
+# Unauthorized copying of this file, via any medium is strictly prohibited
+# Proprietary and confidential
+
+python3 ~/gen.py -i $1
+rm gdb.out
+mv gdb.svg /home/autoshare/pei.tang/
